2a108e4e7700e02ccacd13cd7c7f2598514e790f
József Sallai <jozsef@sallai.me>
2025-12-28 14:08:17 +0200

    update deps and use prisma trigger extension

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 
 export default defineConfig({
   project: "proj_kziwfrqkxqojvirsmuio",
@@ -16,4 +17,11 @@ export default defineConfig({
     },
   },
   dirs: ["./src/trigger"],
+  build: {
+    extensions: [
+      prismaExtension({
+        mode: "modern",
+      }),
+    ],
+  },
 });
